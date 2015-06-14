4735535a47d6ada76683eda53f8539a13ec12aea
mememaxgamer <mememaxgamer@gmail.com>
2015-06-14 17:55:09 -0300

    Update launcher.user.js

--- launcher.user.js
+++ launcher.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Launcher
 // @namespace   AposLauncher
-// @include     http://agar.io/
+// @include     https://agar.io/
 // @version     2.1
 // @grant       none
 // @author      http://www.twitch.tv/apostolique
